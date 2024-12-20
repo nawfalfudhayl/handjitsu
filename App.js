10b60f39b7b7b870407989dc927e549bfd55bc14
Fajar Krisna Jaya <fajar.krisna.jaya-2020@ftmm.unair.ac.id>
2024-12-20 18:44:41 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,22 +1,80 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './components/LoginScreen';
-import SignUpScreen from './components/SignUpScreen';
-import ChoosePlayersScreen from './components/ChoosePlayersScreen';
-import GameScreen from './components/GameScreen';
-
-const Stack = createStackNavigator();
-
+import { StatusBar } from 'expo-status-bar';
+import {
+   StyleSheet,
+    Text, 
+    View,
+    TextInput,
+   TouchableOpacity, 
+   Image } from 'react-native';
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Axios from 'axios';
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="ChoosePlayers" component={ChoosePlayersScreen} />
-        <Stack.Screen name="Game" component={GameScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <TouchableOpacity> <Image source={require('./assets/logo.png')} style={styles.logo} /></TouchableOpacity>
+      <HelloWorld name="Fajar Krisna Jssaya"/>
+      <TextInput 
+        style={styles.input} 
+        placeholder="Email" 
+        placeholderTextColor="#aaa" 
+        />
+      <TextInput 
+        style={styles.input} 
+        placeholder="Password" 
+        placeholderTextColor="#aaa" 
+        secureTextEntry={true} 
+      />
+      <Input text="Email" />
+      <Button text="Login" bgcolor="#4DB6AC" />
+      <StatusBar style="hidden"/>
+
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    resizeMode:'stretch',
+  },
+  logo: {
+    // width: 100,
+    //height: 100,
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    backgroundColor: '#f9f9f9',
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#4DB6AC',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 15,
+    width: '100%',
+    alignItems: 'center',
+
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
